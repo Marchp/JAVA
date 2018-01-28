d70ce4b672561d2972660306a79bfcea9a0a574c
Marchp <Marchp0414@Gmail.com>
2018-01-28 16:06:51 +0800

    wenjian

---
+++ TestStream/src/TestFileOutputStream.java
@@ -0,0 +1,27 @@
+import java.io.*;
+public class TestFileOutputStream {
+
+		public static  void main(String [] args)
+		{
+			int b=0;
+			FileInputStream in=null;
+			FileOutputStream out=null;
+			
+			try{
+				in=new FileInputStream("F:/Eclipse/Workspace/CoreJava/src/BubbleSort.java");
+				out=new FileOutputStream("C:/Users/March/Desktop/Test.java");
+				while((b=in.read())!=-1){
+					out.write(b);
+				}
+				in.close();
+				out.close();
+			} catch (FileNotFoundException e2){
+			System.out.println("找不到复制文件!");
+			System.exit(-1);
+			} catch (IOException e1){
+			System.out.println("文件复制错误!");	
+			System.exit(-1);
+			}
+			System.out.println("文件已经成功复制!");	
+		}
+}
---
+++ TestStream/src/TestFileReader.java
@@ -0,0 +1,24 @@
+import java.io.*;
+public class TestFileReader {
+
+	public static void main(String [] args)
+	{
+		int a=0;
+		FileReader fr=null;
+		try{
+			fr=new FileReader("F:/Eclipse/Workspace/CoreJava/src/BubbleSort.java");
+			int num=0;
+			while((a=fr.read())!=-1){
+			System.out.println((char)a);
+			num++;
+			}
+			fr.close();
+			System.out.println("一共读取了"+num+"个字符");
+		} catch (FileNotFoundException e){
+			System.out.println("找不到指定文件!");
+        } catch (IOException e){
+			System.out.println("文件读取错误!");
+		}
+		
+	}
+}
